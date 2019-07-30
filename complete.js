d9603b77321e5caf0cb3fa6a9178263e106dddba
Frank Bruzzese <bruzzese.frank@gmail.com>
2019-07-29 19:32:07 -0700

    feat: add shell of complete page

---
+++ complete.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Ogame AI
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://s160-en.ogame.gameforge.com/game/index.php
+// @grant        none
+// ==/UserScript==
+
+;(function() {
+    console.log('its working')
+})()
