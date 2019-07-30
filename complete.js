fa24926329cdace899fe7da5da9d77205e6c537b
Frank Bruzzese <bruzzese.frank@gmail.com>
2019-07-29 20:11:11 -0700

    chore: add params to complete.js

--- complete.js
+++ complete.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ogame AI
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://s160-en.ogame.gameforge.com/game/index.php*
@@ -9,5 +9,18 @@
 // ==/UserScript==
 
 ;(function() {
-    console.log('its working')
+    const goToUrl = page => {
+        location.href = `/game/index.php?page=${page}`
+    }
+
+    const params = location.search
+    .split("?")[1]
+    .split("&")
+    .reduce((a, c) => {
+      const [key, value] = c.split("=")
+      a[key] = value
+      return a
+    }, {})
+
+    console.log(`You are on page ${params.page}`, params)
 })()
