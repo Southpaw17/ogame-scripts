a48bd6969d125e01668e2ac359e821f86a7b9f33
Frank Bruzzese <bruzzese.frank@gmail.com>
2019-07-29 19:49:57 -0700

    chore: update version

--- complete.js
+++ complete.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ogame AI
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://s160-en.ogame.gameforge.com/game/index.php*
