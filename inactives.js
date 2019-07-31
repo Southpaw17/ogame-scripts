8ef63f4f4a5fad844081d19668070aff03d9a372
Frank Bruzzese <bruzzese.frank@gmail.com>
2019-07-30 19:48:33 -0700

    feat: fetch universe data in inactives.js

--- inactives.js
+++ inactives.js
@@ -9,106 +9,92 @@
 // ==/UserScript==
 
 ;(function() {
-  const pipe = (...args) => a => args.reduce((y, f) => f(y), a)
   const url = path => `https://s160-en.ogame.gameforge.com/api/${path}`
   const fetchAndParse = uri =>
     fetch(url(uri))
       .then(res => res.text())
       .then(text => $($.parseXML(text)))
 
-  Promise.all([fetchHighcores(), fetchPlayers()]).then(
-    ([highscores, players]) => {
-      const idToKey = (acc, curr) => {
-        acc[curr.id] = { ...curr, ...acc[curr.id] }
-        return acc
-      }
-
-      const thing = highscores.reduce(idToKey, {})
-      const output = Object.values(players.reduce(idToKey, thing))
+  Promise.all([fetchUniverse(), fetchHighcores(), fetchPlayers()]).then(
+    ([planets, highscores, players]) => {
+      const playersToScout = players
         .filter(player => player.status === "i")
-        .filter(player => player.position <= 500)
+        .filter(player => highscores[player.id] <= 500)
+        .map(player => player.id)
 
-      Promise.all(
-        output.map(player => fetchAndParse(`playerData.xml?id=${player.id}`))
-      )
-        .then(players => {
-          const t = []
-          players.map(element =>
-            element
-              .find("planet")
-              .each((_, ele) => t.push(ele.getAttribute("coords")))
+      const planetsToScout = planets
+        .filter(planet => playersToScout.indexOf(planet.id) !== -1)
+        .filter(planet => parseInt(planet.coords.split(':')[0]) <= 3)
+        .map(planet => planet.coords)
+        .sort((a, b) => {
+          const [aG, aS, aP] = a.split(":").map(x => parseInt(x))
+          const [bG, bS, bP] = b.split(":").map(x => parseInt(x))
+
+          if (aG > bG) return 1
+          if (aG < bG) return -1
+          if (aG === bG) {
+            if (aS > bS) return 1
+            if (aS < bS) return -1
+            if (aS === bS) {
+              if (aP > bP) return 1
+              if (aP < bP) return -1
+              if (aP === bP) {
+                return 0
+              }
+            }
+          }
+        })
+
+      let index = 0
+
+      const intervalId = setInterval(() => {
+        const probeCount = parseInt($("#probeValue").text())
+        const [used, total] = $("#slotValue")
+          .text()
+          .split("/")
+          .map(t => parseInt(t))
+
+        if (localStorage.getItem("fleetStatus") === "attacking") {
+          if (used === 0) localStorage.removeItem("fleetStatus")
+          $(".btn_blue")[0].click()
+          return console.log(
+            "Fleet is currently attacking, doing nothing for now"
           )
+        }
 
-          t.sort((a, b) => {
-            const [aG, aS, aP] = a.split(":").map(x => parseInt(x))
-            const [bG, bS, bP] = b.split(":").map(x => parseInt(x))
+        if (used < total && probeCount >= 8) {
+          const [galaxy, sector, planet] = planetsToScout[index].split(":")
+          console.log(
+            `[${index}/${planetsToScout.length}]: Sending Probes to ${planetsToScout[index]}`
+          )
+          window.sendShips(6, galaxy, sector, planet, 1, 8)
+          index++
+        } else {
+          $(".btn_blue")[0].click()
+        }
 
-            if (aG > bG) return 1
-            if (aG < bG) return -1
-            if (aG === bG) {
-              if (aS > bS) return 1
-              if (aS < bS) return -1
-              if (aS === bS) {
-                if (aP > bP) return 1
-                if (aP < bP) return -1
-                if (aP === bP) {
-                  return 0
-                }
-              }
-            }
-          })
-
-          return t
-        })
-        .then(coords => {
-          let index = 0
-
-          const intervalId = setInterval(() => {
-            const probeCount = parseInt($("#probeValue").text())
-            const [used, total] = $("#slotValue")
-              .text()
-              .split("/")
-              .map(t => parseInt(t))
-
-              if (localStorage.getItem('fleetStatus') === 'attacking') {
-                if (used === 0) localStorage.removeItem('fleetStatus')
-                $('.btn_blue')[0].click()
-                return console.log('Fleet is currently attacking, doing nothing for now')
-              }
-
-            if (used < total && probeCount >= 8) {
-              const [galaxy, sector, planet] = coords[index].split(":")
-              console.log(
-                `[${index}/${coords.length}]: Sending Probes to ${
-                  coords[index]
-                }`
-              )
-              window.sendShips(6, galaxy, sector, planet, 1, 8)
-              index++
-            } else {
-              $(".btn_blue")[0].click()
-            }
-
-            if (index === coords.length) {
-              clearInterval(intervalId)
-              console.log("Work Complete!")
-              setTimeout(() => location.href = '/game/index.php?page=messages', 60000)
-            }
-          }, 1000)
-        })
+        if (index === planetsToScout.length) {
+          clearInterval(intervalId)
+          console.log("Work Complete!")
+          setTimeout(
+            () => (location.href = "/game/index.php?page=messages"),
+            60000
+          )
+        }
+      }, 1000)
     }
   )
 
   function fetchHighcores() {
     return fetchAndParse("highscore.xml?category=1&type=0").then(scores => {
-      const arr = []
+      const arr = {}
 
-      scores.find("player").each((_, element) =>
-        arr.push({
-          id: element.getAttribute("id"),
-          position: element.getAttribute("position")
-        })
-      )
+      scores
+        .find("player")
+        .each(
+          (_, player) =>
+            (arr[player.getAttribute("id")] = player.getAttribute("position"))
+        )
 
       return arr
     })
@@ -123,6 +109,21 @@
           id: player.getAttribute("id"),
           name: player.getAttribute("name"),
           status: player.getAttribute("status")
+        })
+      )
+
+      return arr
+    })
+  }
+
+  function fetchUniverse() {
+    return fetchAndParse("universe.xml").then(universe => {
+      const arr = []
+
+      universe.find("planet").each((_, planet) =>
+        arr.push({
+          id: planet.getAttribute("player"),
+          coords: planet.getAttribute("coords")
         })
       )
 
