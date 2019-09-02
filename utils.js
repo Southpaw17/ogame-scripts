df8d2dd11b554421e61a8970521fc83dc71fff60
Frank Bruzzese <bruzzese.frank@gmail.com>
2019-09-02 11:12:28 -0700

    feat: add utils.js

---
+++ utils.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Ogame AI - Utils
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Global Utility Methods that don't have another home
+// @author       You
+// @match        https://s160-en.ogame.gameforge.com/game/index.php*
+// @grant        none
+// ==/UserScript==
+
+;(function() {
+    console.log('it worked')
+})()
+  
